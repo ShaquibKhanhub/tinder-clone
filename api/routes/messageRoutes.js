fb39407179d38d2585744a914916d0fac51d5215
Shaquibkhanhub <shaquibkhanrdx34@gmail.com>
2024-10-14 20:20:13 +0530

    backend is complete

--- api/routes/messageRoutes.js
+++ api/routes/messageRoutes.js
@@ -1,5 +1,15 @@
 import express from "express";
+import {
+  getConversation,
+  sendMessage,
+} from "../controllers/messageController.js";
+import { protectRoute } from "../middleware/auth.js";
 
 const router = express.Router();
+
+router.use(protectRoute);
+
+router.post("/send", sendMessage);
+router.get("/conversation/:userId", getConversation);
 
 export default router;
