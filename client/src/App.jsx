4a038c1a671f1786e149836786c6c5f7353e85f5
Shaquibkhanhub <shaquibkhanrdx34@gmail.com>
2024-10-16 22:22:05 +0530

    setting routes for routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AuthPage from "./pages/AuthPage";
+import ProfilePage from "./pages/ProfilePage";
+import ChatPage from "./pages/ChatPage";
+
 function App() {
-  return <>tinder clone</>;
+  return (
+    <div className="absolute inset-0">
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/chat/:id" element={<ChatPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
