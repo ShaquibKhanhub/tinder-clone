817ec40b6f37009c8072160564e3700acbb0e5ac
Shaquibkhanhub <shaquibkhanrdx34@gmail.com>
2024-10-17 22:15:25 +0530

    working on auth-page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import ChatPage from "./pages/ChatPage";
 
 function App() {
   return (
-    <div className="absolute inset-0">
+    <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
